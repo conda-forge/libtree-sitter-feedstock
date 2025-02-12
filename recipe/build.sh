1611701eadc73ff1cf397fb162cc5e1bd4c576f3
Jindřich Makovička <makovick@gmail.com>
2025-02-12 20:46:01 +0100

    Set -D_BSD_SOURCE to enable endian macros

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 unset STRIP
-make AMALGAMATED=1
-make install PREFIX=${PREFIX}
+make AMALGAMATED=1 CFLAGS="${CFLAGS} -D_BSD_SOURCE"
+make install PREFIX=${PREFIX} CFLAGS="${CFLAGS} -D_BSD_SOURCE"
